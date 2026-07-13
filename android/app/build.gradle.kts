76c1e512db3fa5854868849b28c699938a2c3477
Christian Brooker <christianbrooker@gmail.com>
2026-07-13 10:30:50 +1000

    Release: bump iOS and Android builds to 56

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -48,7 +48,7 @@ android {
         applicationId = "com.tacmap"
         minSdk = 26
         targetSdk = 35
-        versionCode = injectedVersionCode ?: 54
+        versionCode = injectedVersionCode ?: 56
         versionName = "1.2.1"
 
         vectorDrawables { useSupportLibrary = true }
