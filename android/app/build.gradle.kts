ecd39ce622eb12a6659d7d02895bfca39378757a
Christian Brooker <christianbrooker@gmail.com>
2026-09-17 17:29:24 +1000

    Prepare German localization release 2.0.1 build 65

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -281,8 +281,8 @@ android {
         applicationId = "com.tacmap"
         minSdk = 26
         targetSdk = 36
-        versionCode = injectedVersionCode ?: 64
-        versionName = "2.0.0"
+        versionCode = injectedVersionCode ?: 65
+        versionName = "2.0.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         vectorDrawables { useSupportLibrary = true }
